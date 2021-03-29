26a0da76e2aa706172f99c7308d0c4164df91d35
tna007 <anh.to@edu.bc.fi>
2021-03-29 13:22:46 +0300

    practice class component
    manipulate state class

--- test-boxes/src/Main.js
+++ test-boxes/src/Main.js
@@ -1,23 +1,86 @@
-import React from "react";
+import React, { Component } from "react";
 
 const Box = (props) => {
+  const sayHi = () => {
+    console.log("clicked");
+  };
   return (
     <div className="box">
       <h2>Name: {props.name}</h2>
       <p>Age: {props.age}</p>
       <p>Title: {props.title}</p>
+      <button onClick={sayHi}>Test</button>
     </div>
   );
 };
 
-const Main = () => {
-  return (
-    <div>
-      <Box name="Anh T" age="25" title="student" />
-      <Box name="Dan R" age="28" title="ceo" />
-      <Box name="Ted K" age="26" title="cfo" />
-    </div>
-  );
-};
+class Main extends Component {
+  state = {
+    persons: [
+      {
+        name: "Anh T",
+        age: 25,
+        title: "student",
+      },
+      {
+        name: "Dan R",
+        age: 28,
+        title: "ceo",
+      },
+      {
+        name: "Ted K",
+        age: 29,
+        title: "cfo",
+      },
+    ],
+  };
+
+  sayHi = () => {
+    this.setState({
+      persons: [
+        {
+          name: "Dara T",
+          age: 25,
+          title: "student",
+        },
+        {
+          name: "Louis R",
+          age: 28,
+          title: "ceo",
+        },
+        {
+          name: "Cindy K",
+          age: 29,
+          title: "cfo",
+        },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <main>
+        <button onClick={this.sayHi}>Hello from main component</button>
+        <div>
+          <Box
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            title={this.state.persons[0].title}
+          />
+          <Box
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            title={this.state.persons[1].title}
+          />
+          <Box
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            title={this.state.persons[2].title}
+          />
+        </div>
+      </main>
+    );
+  }
+}
 
 export default Main;
