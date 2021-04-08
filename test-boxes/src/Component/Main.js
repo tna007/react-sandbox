e9b216deaa38e5ff041e1cf88a74d9aaec0678bc
tna007 <anh.to@edu.bc.fi>
2021-04-08 19:19:30 +0300

    Rename file

--- test-boxes/src/Component/Main.js
+++ test-boxes/src/Component/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import Boxes from "./Boxes";
-import Animals from "./AnimalCard";
+import Animals from "./Animals";
 
 function Home() {
   return (
