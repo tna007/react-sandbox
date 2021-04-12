ac5e9a4125c27569584d1bfaf43c7ec70fd7c7b3
tna007 <anh.to@edu.bc.fi>
2021-04-12 11:59:04 +0300

    Pass circle data to app.js
    Add event handler (start/clear game) to button elements

--- speed-game/src/App.js
+++ speed-game/src/App.js
@@ -4,6 +4,10 @@ import React, { Component } from "react";
 
 import Circle from "./Circle";
 
+const randNum = function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}; // returns a randnum between included min and excluded max
+
 class App extends Component {
   state = {
     circles: [
@@ -25,27 +29,72 @@ class App extends Component {
       },
     ],
     score: 0,
+    current: 0,
   };
 
-  calScore = () => {
+  timer;
+  SPEED = 1000;
+
+  calScore = (id) => {
+    console.log(`You clicked ${id}`);
     this.setState({
       score: this.state.score + 1,
     });
   };
 
+  nextCircle = () => {
+    let nextActive;
+
+    //let current;
+    // do {
+    //   nextActive = randNum(0, 4);
+    // } while (current === nextActive);
+
+    do {
+      nextActive = randNum(1, 5);
+    } while (nextActive === this.state.current);
+
+    // current = nextActive;
+
+    this.setState({
+      current: nextActive,
+    });
+
+    console.log(`Active circle: ${this.state.current}`);
+
+    this.SPEED -= 25;
+    this.timer = setTimeout(this.nextCircle, this.SPEED);
+    console.log("Speed now:", this.SPEED);
+  };
+
+  startGame = () => {
+    this.nextCircle();
+  };
+
+  clearGame = () => {
+    clearTimeout(this.timer);
+  };
+
   render() {
     const circles = this.state.circles.map((circle) => {
-      return <Circle key={circle.id} id={circle.id} color={circle.color} />;
+      return (
+        <Circle
+          key={circle.id}
+          id={circle.id}
+          color={circle.color}
+          click={() => this.calScore(circle.id)}
+        />
+      );
     });
     return (
       <div className="App">
         <h1>Speed Game</h1>
         <p>Score: {this.state.score}</p>
-        <div className="circleGroup" onClick={this.calScore}>
+        <div className="circleGroup" /* onClick={this.calScore} */>
           {circles}
         </div>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={this.startGame}>Start</button>
+        <button onClick={this.clearGame}>Stop</button>
       </div>
     );
   }
--- speed-game/src/Circle.js
+++ speed-game/src/Circle.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Circle(props) {
-  const clicked = () => {
-    console.log(`You clicked ${props.id}`);
-  };
-  return <div className={`circle ${props.color}`} onClick={clicked}></div>;
+  // const clicked = () => {
+  //   console.log(`You clicked ${props.id}`);
+  // };
+  return (
+    <div
+      className={`circle ${props.color}`}
+      onClick={props.click} /* onClick={clicked} */
+    ></div>
+  ); // style={{backgroundColor: props.color}}
 }
 
 export default Circle;
