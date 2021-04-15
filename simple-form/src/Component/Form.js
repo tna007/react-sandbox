b93285e90252eb564b32baa25a62090d8524c3d3
tna007 <anh.to@edu.bc.fi>
2021-04-15 15:48:55 +0300

    Passing changeHandler to Form component

--- simple-form/src/Component/Form.js
+++ simple-form/src/Component/Form.js
@@ -1,29 +1,58 @@
 import React, { Component } from "react";
 
-function Form() {
+function Form(props) {
   return (
-    <form id="form">
-      <div id="form-line">
-        <label for="firstName">First name</label>
-        <input type="text" name="firstName" />
-      </div>
-      <div id="form-line">
-        <label for="lastName">Last name</label>
-        <input type="text" name="lastName" />
-      </div>
+    <main>
+      <form id="form">
+        <div id="form-line">
+          <label htmlFor="firstName">First name</label>
+          <input
+            type="text"
+            name="firstName"
+            onChange={props.value}
+            placeholder="First name"
+            required
+          />
+        </div>
+        <div id="form-line">
+          <label htmlFor="lastName">Last name</label>
+          <input
+            type="text"
+            name="lastName"
+            onChange={props.value}
+            placeholder="Last name"
+            required
+          />
+        </div>
 
-      <div id="form-line">
-        <label for="phone">Phone number</label>
-        <input type="text" name="phone" />
-      </div>
+        <div id="form-line">
+          <label htmlFor="phone">Phone number</label>
+          <input
+            type="tel"
+            name="phoneNumber"
+            onChange={props.value}
+            placeholder="Enter phone number"
+          />
+        </div>
 
-      <div id="form-line">
-        <label for="role">Role</label>
-        <input type="select" name="role"></input>
-      </div>
-      <div className="button"></div>
-      <button type="submit">Send</button>
-    </form>
+        <div id="form-line">
+          <label htmlFor="role">Role</label>
+          <input type="select" name="role" onChange={props.value}></input>
+        </div>
+
+        <div id="form-line">
+          <label htmlFor="msg">Message</label>
+          <textarea
+            id="msg"
+            name="message"
+            onChange={props.value}
+            placeholder="Enter your message"
+          ></textarea>
+        </div>
+        <div className="button"></div>
+        <button type="submit">Send</button>
+      </form>
+    </main>
   );
 }
 
